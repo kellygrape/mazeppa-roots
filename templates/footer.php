dc43c5d1a4b11693d658f577679706faf8c7cba2
Kelly Anne <kpipe@sju.edu>
2014-02-13 18:19:17 -0500

    Working with the footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,42 @@
+<section class="footer-widgets">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <?php dynamic_sidebar('footer-left'); ?>
+      </div>
+      <div class="col-md-4">
+        <?php dynamic_sidebar('footer-center'); ?>
+      </div>
+      <div class="col-md-4">
+        <div class="widget social-buttons">
+        <h3>Connect with us</h3>
+          <a href="https://www.facebook.com/mazeppaprod">
+            <span class="fa-stack fa-lg">
+              <i class="fa fa-circle fa-stack-2x"></i>
+              <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+            </span>
+          </a>
+          <a href="https://twitter.com/mazeppaprod">
+            <span class="fa-stack fa-lg">
+              <i class="fa fa-circle fa-stack-2x"></i>
+              <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+            </span>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
 <footer class="content-info container" role="contentinfo">
   <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="col-sm-6">
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    </div>
+    <div class="col-sm-6">
+      <ul class="nav nav-pills">
+        <li><a href="https://www.ticketturtle.com/index.php?ticketing=mazep">Buy Tickets</a></li>
+      </ul>
     </div>
   </div>
 </footer>
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -10,11 +10,10 @@
 	    <a href="https://www.facebook.com/mazeppaprod"><span class="fa-stack fa-lg">
 		  <i class="fa fa-circle fa-stack-2x"></i>
 		  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-		</span></a>
-		<a href="https://twitter.com/mazeppaprod"><span class="fa-stack fa-lg">
+      </span></a>
+      <a href="https://twitter.com/mazeppaprod"><span class="fa-stack fa-lg">
 		  <i class="fa fa-circle fa-stack-2x"></i>
-		  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-		</span></a>
+		  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></a>
 	  </span>
     </div>
   </div>
